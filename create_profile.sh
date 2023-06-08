55ebe576045a65894543b9f8af71e41477847e0f
Daniel Azevedo <daniazevedo77@gmail.com>
2023-06-08 21:40:24 +0100

    scripts update

--- create_profile.sh
+++ create_profile.sh
@@ -4,4 +4,3 @@ cp ~/.profile ~/profile
 echo "exec slstatus & exec xautolock -time 3 -locker slock & exec picom & exec dwm" >> .profile
 clear
 echo Now logout and reenter with your display manager.
-
--- create_xinitrc.sh
+++ create_xinitrc.sh
@@ -4,4 +4,3 @@ cp ~/.xinitrc ~/xinitrc
 echo "exec slstatus & exec xautolock -time 3 -locker slock & exec picom & exec dwm" >> .xinitrc
 clear
 echo Now logout and type startx
-
