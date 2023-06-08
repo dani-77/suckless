b3695a8e146538f5934fce3b5b70dfbfbf23b93c
Daniel Azevedo <49279228+dani-77@users.noreply.github.com>
2023-06-08 21:28:04 +0100

    Update create_profile.sh

--- create_profile.sh
+++ create_profile.sh
@@ -1,7 +1,7 @@
 #%/bin/bash
-cd ..
+cd ~/
 cp ~/.profile ~/profile
-echo "exec slstatus & exec xautolock -time 3 -locker slock & exec picom & exec dwm" >> .xinitrc
+echo "exec slstatus & exec xautolock -time 3 -locker slock & exec picom & exec dwm" >> .profile
 clear
 echo Now logout and reenter with your display manager.
 
