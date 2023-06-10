bc78b7af465fdd1904def0af4e2a766a7adfde5c
Daniel Azevedo <49279228+dani-77@users.noreply.github.com>
2023-06-10 19:23:44 +0100

    Update create_profile.sh

--- create_profile.sh
+++ create_profile.sh
@@ -1,4 +1,4 @@
-#%/bin/bash
+#!/bin/bash
 cd ~/
 cp ~/.profile ~/profile
 echo "exec feh --bg-fill ~/dwm_linux-wallpaper-1366x768.jpg & exec slstatus & exec xautolock -time 3 -locker slock & exec picom & exec dwm" >> .profile
