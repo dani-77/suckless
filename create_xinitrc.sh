6eecd9ed90b1274716e2cf480b6dec530122dad3
Daniel Azevedo <49279228+dani-77@users.noreply.github.com>
2023-06-10 19:24:06 +0100

    Update create_xinitrc.sh

--- create_xinitrc.sh
+++ create_xinitrc.sh
@@ -1,4 +1,4 @@
-#%/bin/bash
+#!/bin/bash
 cd ~/
 cp ~/.xinitrc ~/xinitrc
 echo "exec feh --bg-fill ~/dwm_linux-wallpaper-1366x768.jpg & exec slstatus & exec xautolock -time 3 -locker slock & exec picom & exec dwm" >> .xinitrc
