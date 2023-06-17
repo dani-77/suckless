977bc39477b9e1c36c348e01dd02930920349fee
Daniel Azevedo <49279228+dani-77@users.noreply.github.com>
2023-06-17 11:23:59 +0100

    Update create_profile.sh

--- create_profile.sh
+++ create_profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/
 cp ~/.profile ~/profile
-echo "exec feh --bg-fill ~/dwm_linux-wallpaper-1366x768.jpg & exec slstatus & exec xautolock -time 3 -locker slock & exec picom & exec dwm" >> .profile
+echo "exec feh --bg-fill ~/dwm-black-white.png & exec slstatus & exec xautolock -time 3 -locker slock & exec picom & exec dwm" >> .profile
 clear
 echo Now logout and reenter with your display manager.
